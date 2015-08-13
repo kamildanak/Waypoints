10e0dba106920eb92c5290c0764c1d3cc674e472
Kamil Danak <kamilx3@gmail.com>
2015-08-14 01:30:25 +0200

    Rewritten block placement

--- src/main/java/info/jbcs/minecraft/waypoints/item/ItemWaypoint.java
+++ src/main/java/info/jbcs/minecraft/waypoints/item/ItemWaypoint.java
@@ -1,5 +1,6 @@
 package info.jbcs.minecraft.waypoints.item;
 
+import info.jbcs.minecraft.waypoints.Waypoints;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -41,51 +42,43 @@ public class ItemWaypoint extends ItemBlock {
                     break;
             }
         }
+        if (stack.stackSize == 0) return false;
+        if (!player.canPlayerEdit(x, y, z, side, stack)) return false;
+        if (!world.canPlaceEntityOnSide(blockWaypoint, x, y, z, false, side, player, stack)) return false;
 
-        if (stack.stackSize == 0) {
-            return false;
-        } else if (!player.canPlayerEdit(x, y, z, side, stack)) {
-            return false;
-        } else if (world.canPlaceEntityOnSide(blockWaypoint, x, y, z, false, side, player, stack)) {
-            boolean exp = world.getBlock(x + 1, y, z) == blockWaypoint;
-            boolean expp = world.getBlock(x + 2, y, z) == blockWaypoint;
-            boolean exppp = world.getBlock(x + 3, y, z) == blockWaypoint;
-            boolean exm = world.getBlock(x - 1, y, z) == blockWaypoint;
-            boolean exmm = world.getBlock(x - 2, y, z) == blockWaypoint;
-            boolean exmmm = world.getBlock(x - 3, y, z) == blockWaypoint;
-            boolean ezp = world.getBlock(x, y, z + 1) == blockWaypoint;
-            boolean ezpp = world.getBlock(x, y, z + 2) == blockWaypoint;
-            boolean ezppp = world.getBlock(x, y, z + 3) == blockWaypoint;
-            boolean ezm = world.getBlock(x, y, z - 1) == blockWaypoint;
-            boolean ezmm = world.getBlock(x, y, z - 2) == blockWaypoint;
-            boolean ezmmm = world.getBlock(x, y, z - 3) == blockWaypoint;
+        int north = countWaypointBlocks(world, x, y, z, 0, 0, 1, Waypoints.maxSize-1);
+        int south = countWaypointBlocks(world, x, y, z, 0, 0, -1, Waypoints.maxSize-1);
+        int east = countWaypointBlocks(world, x, y, z, 1, 0, 0, Waypoints.maxSize-1);
+        int west = countWaypointBlocks(world, x, y, z, -1, 0, 0, Waypoints.maxSize-1);
 
-            int mexp = world.getBlockMetadata(x + 1, y, z);
-            int mexm = world.getBlockMetadata(x - 1, y, z);
-            int mezp = world.getBlockMetadata(x, y, z + 1);
-            int mezm = world.getBlockMetadata(x, y, z - 1);
+        if(north==-1 || south==-1 || east==-1 || west==-1) return false;
+        if(north+south+1>Waypoints.maxSize || east+west+1>Waypoints.maxSize) return false;
+        if(isActivated(world,x+1,y,z) || isActivated(world,x-1,y,z) ||
+                isActivated(world,x,y,z+1) || isActivated(world,x,y,z-1)) return false;
 
+        Block block = blockWaypoint;
+        int oldMeta = this.getMetadata(stack.getItemDamage());
+        int meta = blockWaypoint.onBlockPlaced(world, x, y, z, side, hx, hy, hz, oldMeta);
 
-            if ((exp && expp && exppp) || (exp && expp && exm) || (exp && exm && exmm) || (exm && exmm && exmmm))
-                return false;
-            if ((ezp && ezpp && ezppp) || (ezp && ezpp && ezm) || (ezp && ezm && ezmm) || (ezm && ezmm && ezmmm))
-                return false;
-            if ((mexp != 0 && exp) || (mexm != 0 && exm) || (mezp != 0 && ezp) || (mezm != 0 && ezm)) return false;
+        int ox = x, oz = z;
+        while (world.getBlock(ox - 1, y, oz) == blockWaypoint) ox--;
+        while (world.getBlock(ox, y, oz - 1) == blockWaypoint) oz--;
 
-            Block block = blockWaypoint;
-            int oldMeta = this.getMetadata(stack.getItemDamage());
-            int meta = blockWaypoint.onBlockPlaced(world, x, y, z, side, hx, hy, hz, oldMeta);
-
-            int ox = x, oz = z;
-            while (world.getBlock(ox - 1, y, oz) == blockWaypoint) ox--;
-            while (world.getBlock(ox, y, oz - 1) == blockWaypoint) oz--;
-
-            if (placeBlockAt(stack, player, world, x, y, z, side, hx, hy, hz, meta)) {
-                world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, block.stepSound.getBreakSound(), (block.stepSound.getVolume() + 1.0F) / 2.0F, block.stepSound.getPitch() * 0.8F);
-                --stack.stackSize;
-            }
+        if (placeBlockAt(stack, player, world, x, y, z, side, hx, hy, hz, meta)) {
+            world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, block.stepSound.getBreakSound(), (block.stepSound.getVolume() + 1.0F) / 2.0F, block.stepSound.getPitch() * 0.8F);
+            --stack.stackSize;
         }
+
         return true;
+    }
+    int countWaypointBlocks(World world, int x, int y, int z, int px, int py, int pz, int maxSize){
+        for(int c=0; c<maxSize+1; c++){
+            if(world.getBlock(x+(c+1)*px,y+(c+1)*py,z+(c+1)*pz)!=blockWaypoint) return c;
+        }
+        return -1;
+    }
+    boolean isActivated(World world, int x, int y, int z){
+        return world.getBlockMetadata(x, y, z) != 0 && world.getBlock(x, y, z)==blockWaypoint;
     }
 
 }
