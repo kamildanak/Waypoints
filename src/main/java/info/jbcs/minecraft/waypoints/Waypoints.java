9e26d43fa51b9129595c47da8cdc6807ea724109
Kamil Danak <kamildanak@gmail.com>
2016-04-01 15:21:02 +0200

    Version bump

--- src/main/java/info/jbcs/minecraft/waypoints/Waypoints.java
+++ src/main/java/info/jbcs/minecraft/waypoints/Waypoints.java
@@ -29,7 +29,7 @@ import static net.minecraftforge.fml.common.registry.GameRegistry.addRecipe;
 public class Waypoints {
     public static final String MODID = "Waypoints";
     public static final String MODNAME = "Waypoints";
-    public static final String VERSION = "1.8.9-1.1.1";
+    public static final String VERSION = "1.8.9-1.2.0";
     public static boolean compactView;
     public static boolean craftable;
     public static boolean allowActivation;
