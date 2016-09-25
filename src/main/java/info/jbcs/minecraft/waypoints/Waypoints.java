0b5eebf24e10685fd308e5f8a444d9effcedc4a8
Kamil Danak <kamildanak@gmail.com>
2016-09-25 10:25:29 +0200

    Moved config loading to loadConfigOptions() function

--- src/main/java/info/jbcs/minecraft/waypoints/Waypoints.java
+++ src/main/java/info/jbcs/minecraft/waypoints/Waypoints.java
@@ -75,24 +75,12 @@ public class Waypoints {
         blockWaypoint = new BlockWaypoint();
         GameRegistry.registerBlock(blockWaypoint, ItemWaypoint.class, "waypoint");
         proxy.init();
-        compactView = config.get("general", "compact view", true, "Only show one line in Waypoint GUI, in order to fit more waypoints on the screen").getBoolean();
-        craftable = config.get("general", "craftable", true, "Set to false to completely disable crafting recipe").getBoolean();
+        loadConfigOptions();
+
         if (craftable)
             addRecipe(new ItemStack(blockWaypoint, 1), "SSS", "SES", 'S', Blocks.STONE, 'E', Items.ENDER_PEARL);
-        allowActivation = config.get("general", "can_no_ops_activate", true, "If set to false only ops can enable Waypoins").getBoolean();
-        playSounds = config.get("general", "play sounds", true, "Set to false to disable teleportation sounds").getBoolean();
-        playSoundEnderman = config.get("general", "play sound enderman", true, "Set to false to play custom sound").getBoolean();
-        maxSize = config.get("general", "max size", 3, "Set maximum size of waypoints (default 3)").getInt();
-        minSize = config.get("general", "min size", 2, "Set minimum size of waypoints (default 2)").getInt();
-        allowNotSquare = config.get("general", "allow not square", false, "Set to true to allow not square (rectangular) waypoints").getBoolean();
-        // Effects and exhaustion
-        teleportationExhaustion = config.get("general", "teleportationExhaustion", 20, "Exhaustion caused by using waypoint").getInt();
-        int[] effects = config.get("general", "effects ids", new int[]{9, 9, 15}, "List of effectts id").getIntList();
-        int[] effectsDurations = config.get("general", "effects durations", new int[]{20, 30, 25}, "List off durations").getIntList();
-        potionEffectsChances = config.get("general", "effects chances", new int[]{30, 10, 5}, "List of probabilities (0-100)% that effect will appear").getIntList();
-        potionEffects = new PotionEffect[Math.min(effects.length, effectsDurations.length)];
-        for (int i = 0; i < potionEffects.length; i++)
-            potionEffects[i] = new PotionEffect(Potion.getPotionById(effects[i]), effectsDurations[i] * 10);
+
+
         if (playSoundEnderman)
             soundEvent = SoundEvents.ENTITY_ENDERMEN_TELEPORT;
         else {
@@ -101,7 +89,6 @@ public class Waypoints {
             SoundEvent.REGISTRY.register(soundEventId++, resourcelocation, new SoundEvent(resourcelocation));
             soundEvent = (SoundEvent) SoundEvent.REGISTRY.getObject(resourcelocation);
         }
-        commonDiscoveryList = config.get("general", "commonDiscoveryList", false, "Share discovered Waypoints between players").getBoolean();
         //
         MinecraftForge.EVENT_BUS.register(this);
         config.save();
@@ -152,6 +139,41 @@ public class Waypoints {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void loadConfigOptions()
+    {
+        compactView = config.get("general", "compact view", true,
+                "Only show one line in Waypoint GUI, in order to fit more waypoints on the screen").getBoolean();
+        craftable = config.get("general", "craftable", true,
+                "Set to false to completely disable crafting recipe").getBoolean();
+        allowActivation = config.get("general", "can_no_ops_activate", true,
+                "If set to false only ops can enable Waypoins").getBoolean();
+        playSounds = config.get("general", "play sounds", true,
+                "Set to false to disable teleportation sounds").getBoolean();
+        playSoundEnderman = config.get("general", "play sound enderman", true,
+                "Set to false to play custom sound").getBoolean();
+        maxSize = config.get("general", "max size", 3,
+                "Set maximum size of waypoints (default 3)").getInt();
+        minSize = config.get("general", "min size", 2,
+                "Set minimum size of waypoints (default 2)").getInt();
+        allowNotSquare = config.get("general", "allow not square", false,
+                "Set to true to allow not square (rectangular) waypoints").getBoolean();
+        teleportationExhaustion = config.get("general", "teleportationExhaustion", 20,
+                "Exhaustion caused by using waypoint").getInt();
+        commonDiscoveryList = config.get("general", "commonDiscoveryList", false,
+                "Share discovered Waypoints between players").getBoolean();
+
+        // Effects and exhaustion
+        int[] effects = config.get("general", "effects ids", new int[]{9, 9, 15},
+                "List of effects id").getIntList();
+        int[] effectsDurations = config.get("general", "effects durations", new int[]{20, 30, 25},
+                "List off durations").getIntList();
+        potionEffectsChances = config.get("general", "effects chances", new int[]{30, 10, 5},
+                "List of probabilities (0-100)% that effect will appear").getIntList();
+        potionEffects = new PotionEffect[Math.min(effects.length, effectsDurations.length)];
+        for (int i = 0; i < potionEffects.length; i++)
+            potionEffects[i] = new PotionEffect(Potion.getPotionById(effects[i]), effectsDurations[i] * 10);
     }
 
 }
